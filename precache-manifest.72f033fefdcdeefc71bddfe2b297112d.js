021cdd80e4f79ff567bec6cb22e5fd5089578bfa
Moo <moyaying88@gmail.com>
2019-02-28 10:47:02 +0800

    Updates

---
+++ precache-manifest.72f033fefdcdeefc71bddfe2b297112d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d493bafe6c02d971188c",
+    "url": "/blog/static/css/main.436c9284.chunk.css"
+  },
+  {
+    "revision": "d493bafe6c02d971188c",
+    "url": "/blog/static/js/main.d493bafe.chunk.js"
+  },
+  {
+    "revision": "bcecdc30670b92c225a7",
+    "url": "/blog/static/js/runtime~main.bcecdc30.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/blog/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blog/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7d3d58a16e31649b0b8e3e01234bf61f",
+    "url": "/blog/index.html"
+  }
+];
